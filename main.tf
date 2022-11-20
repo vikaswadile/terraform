597ca99bda85a1c5bdcd84dab0959ce35ab7b62d
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2022-11-20 19:37:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,13 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 } 
+
+
+
+resource "aws_instance" "web" {
+  ami                    = "${var.ami_details}"
+  instance_type          = "${var.instance}"
+  tags = {
+    Name = "${var.name}"
+  }
+}
