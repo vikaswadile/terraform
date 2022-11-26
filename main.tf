4663eec0148a0025ea21b2f2cd8437565b8eaf04
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2022-11-26 20:14:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami                    = "${var.ami}"
   instance_type          = "${var.instance_type}"
   tags = {
-    Name = ${var.instance_name}"
+    Name = "${var.instance_name}"
   }
 }
