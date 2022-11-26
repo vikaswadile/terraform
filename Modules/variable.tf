220bfa90cdc6edab85ca51d5912df1a51e47d281
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2022-11-26 19:31:26 +0530

    Create variable.tf

---
+++ Modules/variable.tf
@@ -0,0 +1,19 @@
+variable "hello" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_ket" {
+  type = string
+}
+
+variable "aws_instance" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
