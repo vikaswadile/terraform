f47ad592e5797f71ae12ea2db768490c735cdf0b
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2022-11-26 20:09:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,23 @@
 variable "hello" {
   type = string
 }
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_ket" {
+  type = string
+}
+
+variable "aws_instance" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
